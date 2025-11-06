7a55d92ae44ccac7d3f42ff7b6e3c5461ef6f656
siddhi-demo <sharmasiddhi2007@gmail.com>
2025-11-06 20:59:33 +0530

    Ready for Deployment

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LandingPage from './pages/landing';
+import Authentication from './pages/authentication';
+import { AuthProvider } from './contexts/AuthContext';
+import VideoMeetComponent from './pages/VideoMeet';
+import HomeComponent from './pages/home';
+import History from './pages/history';
+
+function App(){
+  return (
+    <div className='App'>
+      <Router>
+        <AuthProvider>
+        <Routes>
+
+          {/* <Route path/> */}
+          <Route path='/' element={<LandingPage />} />
+
+          <Route path="/auth" element={<Authentication/>}/>
+
+          <Route path='/home' element={<HomeComponent/>} />
+          <Route path='/history' element={<History />} />
+
+          <Route path='/:url' element={<VideoMeetComponent/>}/>
+
+        </Routes>
+        </AuthProvider>
+
+      </Router>
+    </div>
+    
+  );
+}
+
+export default App;
